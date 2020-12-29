87c2d09385ec00d083ec31573d703fcfafde6779
Pavit <pavitpim@gmail.com>
2020-12-29 22:15:33 +0700

    Finished OOP

---
+++ 7_FCC_OOP/1.Create a Basic JavaScript Object.js
@@ -0,0 +1,4 @@
+let dog = {
+    name : "Afalc",
+    numLegs : 2,
+   };
---
+++ 7_FCC_OOP/14.Remember to Set the Constructor Property when Changing the Prototype.js
@@ -0,0 +1,10 @@
+Dog.prototype = {
+    constructor: Dog, 
+    numLegs: 2,
+    eat: function() {
+      console.log("nom nom nom");
+    },
+    describe: function() {
+      console.log("My name is " + this.name);
+    }
+  };
